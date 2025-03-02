2bbc46d4ba001787516e04bfc486b67920939525
daniellelanna <daniilanaa00@gmail.com>
2025-03-02 13:12:34 -0500

    jsnotwork

---
+++ backend/homepage.js
@@ -0,0 +1,12 @@
+document.getElementById("referral-code").onclick = function() {
+    window.location.href = "codereferral.html";
+  };
+  document.getElementById("sign-in").onclick = function() {
+    window.location.href = "sign-in.html";
+  };
+  document.getElementById("submit").onclick = function() {
+    window.location.href = "memorymatch.html";
+  };
+// document.getElementById("start").onclick = function() {
+//   window.location.href = "[pending].html";
+// };
