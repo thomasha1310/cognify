725b561d083c74e0a6cfba5888889c7e5cdf53e0
Thomas Ha <thomasha.1310@gmail.com>
2025-03-02 10:47:30 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
+require("dotenv").config({ path: "./backend/.env.local" });
+
 // server.js
 const express = require("express");
 const app = express();
 const port = 3000;
+
+// MongoDB setup
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const mongodbUser = process.env.MONGODB_USER;
+const mongodbPassword = process.env.MONGODB_PASSWORD;
+const mongodbCluster = process.env.MONGODB_CLUSTER;
+const uri = `mongodb+srv://${mongodbUser}:${mongodbPassword}@${mongodbCluster}.wwhiw.mongodb.net/?retryWrites=true&w=majority&appName=${mongodbCluster}`;
+console.log(uri);
 
 // Serve static files from the 'public' folder
 app.use(express.static("public"));
@@ -25,3 +35,27 @@ app.post("/api/data", (req, res) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
